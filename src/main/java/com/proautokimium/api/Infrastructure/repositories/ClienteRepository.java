a6bc16d9587f82ef73a8385e12c5c683ffa39119
MurilloHMS <murillohenriquemelo@outlook.com>
2025-08-29 12:07:37 -0300

    feat: include clienteRepository

---
+++ src/main/java/com/proautokimium/api/Infrastructure/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+﻿package com.proautokimium.api.Infrastructure.repositories;
+
+import com.proautokimium.api.domain.entities.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+}
