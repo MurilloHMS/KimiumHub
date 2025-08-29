3ed8d1a89f657401b577b992cdf0382f0cb13293
MurilloHMS <murillohenriquemelo@outlook.com>
2025-08-29 11:57:26 -0300

    refact(clientDTO): update to record

--- src/main/java/com/proautokimium/api/Application/DTOs/cliente/ClienteDTO.java
+++ src/main/java/com/proautokimium/api/Application/DTOs/cliente/ClienteDTO.java
@@ -1,15 +1,3 @@
 package com.proautokimium.api.Application.DTOs.cliente;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class ClienteDTO {
-    private String codParceiro;
-    private String documento;
-    private String nome;
-    private String email;
-    private boolean ativo;
-    private boolean recebeEmail;
-}
+public record ClienteDTO (String codParceiro, String documento, String nome, String email, boolean ativo, boolean recebeEmail){}
