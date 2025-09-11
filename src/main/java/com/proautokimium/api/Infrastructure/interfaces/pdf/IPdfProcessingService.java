0963d6141711965841743b684922b264e8a61f24
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-11 11:38:15 -0300

    feat(pdf): implements services to processing pdf

---
+++ src/main/java/com/proautokimium/api/Infrastructure/interfaces/pdf/IPdfProcessingService.java
@@ -0,0 +1,10 @@
+﻿package com.proautokimium.api.Infrastructure.interfaces.pdf;
+
+import com.proautokimium.api.Application.DTOs.pdf.PdfPageInfo;
+
+import java.util.List;
+
+public interface IPdfProcessingService {
+    List<PdfPageInfo> GetPdfByPage(String inputPdfPath);
+    void SavePages(String inputPdfPath, String outputFolder, List<PdfPageInfo> pages);
+}
---
+++ src/main/java/com/proautokimium/api/Infrastructure/services/pdf/PdfProcessingService.java
@@ -0,0 +1,30 @@
+﻿package com.proautokimium.api.Infrastructure.services.pdf;
+
+import com.proautokimium.api.Application.DTOs.pdf.PdfPageInfo;
+import com.proautokimium.api.Infrastructure.interfaces.pdf.IPdfProcessingService;
+import com.proautokimium.api.Infrastructure.interfaces.pdf.IPdfReader;
+import com.proautokimium.api.Infrastructure.interfaces.pdf.IPdfWriterService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PdfProcessingService implements IPdfProcessingService {
+
+    private final IPdfReader pdfReader;
+    private final IPdfWriterService pdfWriterService;
+
+    public PdfProcessingService(IPdfReader reader, IPdfWriterService writer){
+        this.pdfReader = reader;
+        this.pdfWriterService = writer;
+    }
+    @Override
+    public List<PdfPageInfo> GetPdfByPage(String inputPdfPath) {
+        return pdfReader.getPdfByPage(inputPdfPath);
+    }
+
+    @Override
+    public void SavePages(String inputPdfPath, String outputFolder, List<PdfPageInfo> pages) {
+        pdfWriterService.SavePages(inputPdfPath, outputFolder, pages);
+    }
+}
