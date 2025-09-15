16d0abb8ac815eefe7baa24f259462b3286df92a
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-15 17:19:15 -0300

    feat(product): create product and movement repositories

---
+++ src/main/java/com/proautokimium/api/Infrastructure/repositories/ProductMovementRepository.java
@@ -0,0 +1,14 @@
+package com.proautokimium.api.Infrastructure.repositories;
+
+import com.proautokimium.api.domain.entities.MovementInventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductMovementRepository extends JpaRepository<ProductMovementRepository, UUID> {
+    @Query(value = "SELECT * FROM products_movements WHERE product_id = :id", nativeQuery = true)
+    List<MovementInventory> findMovementByProductId(@Param("id") UUID id);
+}
