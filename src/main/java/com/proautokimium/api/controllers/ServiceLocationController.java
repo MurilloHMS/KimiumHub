897df44ad66be1e8c07f71d79a7707e491b0b5cc
MurilloHMS <murillohenriquemelo@outlook.com>
2025-09-24 16:47:19 -0300

    feat(controller): include put and delete endpoints on ServiceLocationController

--- src/main/java/com/proautokimium/api/controllers/ServiceLocationController.java
+++ src/main/java/com/proautokimium/api/controllers/ServiceLocationController.java
@@ -2,11 +2,15 @@ package com.proautokimium.api.controllers;
 
 import com.proautokimium.api.Application.DTOs.partners.ServiceLocationDTO;
 import com.proautokimium.api.Infrastructure.services.ServiceLocationService;
+import com.proautokimium.api.domain.entities.ServiceLocation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/service-locations")
@@ -25,5 +29,19 @@ public class ServiceLocationController {
     public ResponseEntity<Object> deleteServiceLocation(@RequestParam @NotNull String systemCode) {
         serviceLocationService.deleteServiceLocationBySystemCode(systemCode);
         return ResponseEntity.status(204).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllServiceLocations(){
+        var serviceLocations = serviceLocationService.getAllServiceLocations();
+        return serviceLocations != null
+                ? ResponseEntity.ok(serviceLocations)
+                : ResponseEntity.notFound().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> updateServiceLocations(@RequestBody @NotNull @Valid ServiceLocationDTO serviceLocationDTO){
+        serviceLocationService.updateServiceLocation(serviceLocationDTO);
+        return ResponseEntity.ok().build();
     }
 }
