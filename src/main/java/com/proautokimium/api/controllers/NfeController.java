104d3aff9709600118cf8dd63a73f2c9a04e511f
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-01 10:28:01 -0300

    feat: include nfe controller

---
+++ src/main/java/com/proautokimium/api/controllers/NfeController.java
@@ -0,0 +1,41 @@
+package com.proautokimium.api.controllers;
+
+import com.proautokimium.api.Infrastructure.interfaces.nfe.INfeProcessing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/nfe")
+public class NfeController {
+
+    @Autowired
+    INfeProcessing nfeProcessing;
+
+    @PostMapping( value = "/icms/upload", consumes = "multipart/form-data")
+    public ResponseEntity<byte[]> processIcmsFiles(@RequestParam("files") List<MultipartFile> files) throws Exception{
+        List<InputStream> xmlStreams = new ArrayList<>();
+
+        for(MultipartFile file : files){
+            if(!file.getOriginalFilename().toLowerCase().endsWith(".xml")) continue;
+            xmlStreams.add(file.getInputStream());
+        }
+
+        byte[] excelFile = nfeProcessing.getData(xmlStreams);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=icms.xlsx")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(excelFile);
+    }
+}
