2149c24528e9c3e04c0c92dd0853c5ab7a7a18d0
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-01 11:30:51 -0300

    feat: update nfe interfaces to include nfeData model

--- src/main/java/com/proautokimium/api/Infrastructure/interfaces/nfe/INfeReader.java
+++ src/main/java/com/proautokimium/api/Infrastructure/interfaces/nfe/INfeReader.java
@@ -1,9 +1,12 @@
 package com.proautokimium.api.Infrastructure.interfaces.nfe;
 
+import com.proautokimium.api.domain.models.NfeDataInfo;
 import com.proautokimium.api.domain.models.NfeIcmsInfo;
 
 import java.io.InputStream;
+import java.util.List;
 
 public interface INfeReader {
     NfeIcmsInfo getIcmsByXml(InputStream stream) throws Exception;
+    List<NfeDataInfo> getNfeDataByXml(InputStream stream) throws Exception;
 }
