849f3e8b7098a9e680434767d2bf59c620570d90
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-07 12:36:01 -0300

    chore: removing unused imports

--- src/main/java/com/proautokimium/api/controllers/NewsletterController.java
+++ src/main/java/com/proautokimium/api/controllers/NewsletterController.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
