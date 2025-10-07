6fe2a1c4f54a1dfe6084bda95f29ef12a5b69047
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-07 11:14:48 -0300

    feat: created newsletterDataDTO

---
+++ src/main/java/com/proautokimium/api/Application/DTOs/email/NewsletterData.java
@@ -0,0 +1,10 @@
+package com.proautokimium.api.Application.DTOs.email;
+
+import java.util.List;
+
+import com.proautokimium.api.domain.models.newsletter.NewsletterExchangedParts;
+import com.proautokimium.api.domain.models.newsletter.NewsletterNFeInfo;
+import com.proautokimium.api.domain.models.newsletter.NewsletterServiceOrders;
+import com.proautokimium.api.domain.models.newsletter.NewsletterTechnicalHours;
+
+public record NewsletterData(List<NewsletterNFeInfo> nFeInfos, List<NewsletterServiceOrders> orders, List<NewsletterTechnicalHours> hours, List<NewsletterExchangedParts> parts) {}
