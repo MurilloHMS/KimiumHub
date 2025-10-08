26835c6eca288229c0f166c609bb7b7264abcac7
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-08 08:18:24 -0300

    feat: update regex

--- src/main/java/com/proautokimium/api/domain/valueObjects/Email.java
+++ src/main/java/com/proautokimium/api/domain/valueObjects/Email.java
@@ -11,7 +11,7 @@ public final class Email {
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
             "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*" +
                     "@" +
-                    "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$"
+                    "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{1,7}$"
     );
 
     private String address;
