737bdcbe50ecd015ebdb23e258ca39f0d08c05b2
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-13 09:22:53 -0300

    feat: implements employee CRUD

--- src/main/java/com/proautokimium/api/Infrastructure/repositories/EmployeeRepository.java
+++ src/main/java/com/proautokimium/api/Infrastructure/repositories/EmployeeRepository.java
@@ -1,6 +1,5 @@
 package com.proautokimium.api.Infrastructure.repositories;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.proautokimium.api.domain.entities.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
-	List<Employee> findByCodParceiro(String codParceiro);
+	Employee findByCodParceiro(String codParceiro);
 }
