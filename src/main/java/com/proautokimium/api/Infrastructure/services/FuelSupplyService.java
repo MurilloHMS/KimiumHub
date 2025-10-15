df4605fba85185b0af8c4710c29c4d65b52f6ec8
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-15 09:11:47 -0300

    feat: include fuel supply services

---
+++ src/main/java/com/proautokimium/api/Infrastructure/services/FuelSupplyService.java
@@ -0,0 +1,75 @@
+package com.proautokimium.api.Infrastructure.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.proautokimium.api.Application.DTOs.fuelsupply.FuelSupplyDTO;
+import com.proautokimium.api.Infrastructure.repositories.FuelSupplyRepository;
+import com.proautokimium.api.domain.entities.FuelSupply;
+import com.proautokimium.api.domain.entities.Newsletter;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class FuelSupplyService {
+
+	@Autowired
+	FuelSupplyRepository repository;
+	
+	@Transactional
+	public ResponseEntity<?> createFuelSupply(FuelSupplyDTO dto) {
+		
+		try {
+			FuelSupply fs = new FuelSupply();
+			fs.setActualHodometer(dto.actualHodometer());
+			fs.setAverageKm(dto.averageKm());
+			fs.setDepartment(dto.department());
+			fs.setDiferenceHodometer(dto.diferenceHodometer());
+			fs.setDriverName(dto.driverName());
+			fs.setFuelSupplyDate(dto.fuelSupplyDate());
+			fs.setFuelType(dto.fuelType());
+			fs.setLastHodometer(dto.lastHodometer());
+			fs.setPlate(dto.plate());
+			fs.setPrice(dto.price());
+			fs.setTotalValue(dto.totalValue());
+			
+			repository.save(fs);
+			return ResponseEntity.ok().body("Abastecimento Criado com sucesso!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError()
+					.body("Ocorreu um erro ao criar o abastecimento. Error: " + e.getMessage());
+		}
+	}
+	
+	@Transactional
+	public ResponseEntity<?> insertByRange(List<FuelSupplyDTO> dtoList){
+		try {
+			List<FuelSupply> fuelSupplies = dtoList.stream().map(dto -> {
+				FuelSupply fs = new FuelSupply();
+				fs.setActualHodometer(dto.actualHodometer());
+				fs.setAverageKm(dto.averageKm());
+				fs.setDepartment(dto.department());
+				fs.setDiferenceHodometer(dto.diferenceHodometer());
+				fs.setDriverName(dto.driverName());
+				fs.setFuelSupplyDate(dto.fuelSupplyDate());
+				fs.setFuelType(dto.fuelType());
+				fs.setLastHodometer(dto.lastHodometer());
+				fs.setPlate(dto.plate());
+				fs.setPrice(dto.price());
+				fs.setTotalValue(dto.totalValue());
+				return fs;
+			}).toList();
+			
+			repository.saveAll(fuelSupplies);
+
+	        return ResponseEntity.ok("Abastecimentos inseridos com sucesso!");
+	    } catch (Exception e) {
+	        return ResponseEntity.internalServerError()
+	            .body("Erro ao inserir abastecimentos: " + e.getMessage());
+	    }
+	}
+}
