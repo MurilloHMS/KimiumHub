4a8d0c2da76b08700d55c75535bfddba78f6b95f
MurilloHMS <murillohenriquemelo@outlook.com>
2025-10-29 08:57:12 -0300

    feat: include system code validation

--- src/main/java/com/proautokimium/api/controllers/ProductController.java
+++ src/main/java/com/proautokimium/api/controllers/ProductController.java
@@ -7,6 +7,8 @@ import com.proautokimium.api.Infrastructure.services.inventoryProducts.ProductIn
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,8 +30,12 @@ public class ProductController {
 
     @PostMapping("/inventory/movement")
     public ResponseEntity<Object> createInventoryMovement(@RequestBody @NotNull @Valid ProductMovementDTO dto){
-        inventoryService.includeMovement(dto);
-        return ResponseEntity.status(201).build();
+    	if(dto.systemCode() != null) {
+    		inventoryService.includeMovement(dto);
+            return ResponseEntity.status(201).build();
+    	}else {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Movimentação não incluida, código do sistema está nulo ou vazio");
+    	}
     }
 
     @GetMapping("/inventory/product")
