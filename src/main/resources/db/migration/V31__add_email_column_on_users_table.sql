ea86aedaab8ba2458055c11d75d316bc2688135f
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-03 09:25:15 -0300

    feat(database): add email column to users table with default values and uniqueness constraint

--- src/main/resources/db/migration/V31__add_email_column_on_users_table.sql
+++ src/main/resources/db/migration/V31__add_email_column_on_users_table.sql
@@ -1,2 +1,12 @@
 ALTER TABLE users
-ADD COLUMN email VARCHAR(200) NOT NULL UNIQUE;
+    ADD COLUMN email VARCHAR(200);
+
+UPDATE users
+SET email = CONCAT('user_', id, '@temp.com')
+WHERE email IS NULL;
+
+ALTER TABLE users
+    ALTER COLUMN email SET NOT NULL;
+
+ALTER TABLE users
+    ADD CONSTRAINT users_email_unique UNIQUE (email);
