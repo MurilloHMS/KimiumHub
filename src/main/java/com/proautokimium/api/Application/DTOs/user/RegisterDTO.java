b2a5da2d2db030a1cc122bc65287c1542d00197a
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-03 08:58:41 -0300

    feat(authentication): add DTOs for password management and include email in RegisterDTO

--- src/main/java/com/proautokimium/api/Application/DTOs/user/RegisterDTO.java
+++ src/main/java/com/proautokimium/api/Application/DTOs/user/RegisterDTO.java
@@ -4,4 +4,4 @@ import com.proautokimium.api.domain.enums.UserRole;
 
 import java.util.List;
 
-public record RegisterDTO(String login, String password, List<UserRole> roles) {}
+public record RegisterDTO(String login, String email, String password, List<UserRole> roles) {}
