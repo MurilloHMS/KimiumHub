7f7e39537c9f812e1f306c62ee1af6102863668b
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-24 12:04:01 -0300

    chore: incluído path api no controller

--- src/main/java/com/proautokimium/api/controllers/prostock/ExcelController.java
+++ src/main/java/com/proautokimium/api/controllers/prostock/ExcelController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/excel")
+@RequestMapping("/api/excel")
 public class ExcelController {
 
     private final ExcelService excelService;
