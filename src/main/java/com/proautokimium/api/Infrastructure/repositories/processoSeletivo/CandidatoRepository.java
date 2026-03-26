e8c5c20557e8e2aee02178ab5dcb4085390666f1
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-26 14:44:35 -0300

    feat: declarado novos metodos de busca

--- src/main/java/com/proautokimium/api/Infrastructure/repositories/processoSeletivo/CandidatoRepository.java
+++ src/main/java/com/proautokimium/api/Infrastructure/repositories/processoSeletivo/CandidatoRepository.java
@@ -1,9 +1,13 @@
 package com.proautokimium.api.Infrastructure.repositories.processoSeletivo;
 
+import com.proautokimium.api.domain.entities.processoSeletivo.Candidato;
 import com.proautokimium.api.domain.entities.processoSeletivo.Vaga;
+import com.proautokimium.api.domain.valueObjects.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface CandidatoRepository extends JpaRepository<Vaga, UUID> {
+public interface CandidatoRepository extends JpaRepository<Candidato, UUID> {
+    Optional<Candidato> findByEmail(Email email);
 }
--- src/main/java/com/proautokimium/api/Infrastructure/repositories/processoSeletivo/CandidaturaRepository.java
+++ src/main/java/com/proautokimium/api/Infrastructure/repositories/processoSeletivo/CandidaturaRepository.java
@@ -1,9 +1,12 @@
 package com.proautokimium.api.Infrastructure.repositories.processoSeletivo;
 
+import com.proautokimium.api.domain.entities.processoSeletivo.Candidato;
 import com.proautokimium.api.domain.entities.processoSeletivo.Candidatura;
+import com.proautokimium.api.domain.entities.processoSeletivo.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface CandidaturaRepository extends JpaRepository<Candidatura, UUID> {
+    boolean existsByCandidatoAndVaga(Candidato candidato, Vaga vaga);
 }
