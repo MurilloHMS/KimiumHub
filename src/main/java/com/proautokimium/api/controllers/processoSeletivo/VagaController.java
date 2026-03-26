f1d86e2f14f7c6bd2c49152cd88758e4c4fd31c8
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-26 10:51:25 -0300

    refactor: atualizado controller para utilizar DTOs e UUID

--- src/main/java/com/proautokimium/api/controllers/processoSeletivo/VagaController.java
+++ src/main/java/com/proautokimium/api/controllers/processoSeletivo/VagaController.java
@@ -1,20 +1,24 @@
 package com.proautokimium.api.controllers.processoSeletivo;
 
-import com.proautokimium.api.Infrastructure.repositories.processoSeletivo.VagaRepository;
+import com.proautokimium.api.Application.DTOs.processoSeletivo.vaga.CreateVagaDTO;
+import com.proautokimium.api.Application.DTOs.processoSeletivo.vaga.UpdateVagaDTO;
 import com.proautokimium.api.Infrastructure.services.processoSeletivo.VagaService;
-import com.proautokimium.api.domain.entities.processoSeletivo.Vaga;
+
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/vaga")
 public class VagaController {
 
-    @Autowired
-    private VagaService vagaService;
+    private final VagaService vagaService;
+
+    public VagaController(VagaService vagaService) {
+        this.vagaService = vagaService;
+    }
 
     @GetMapping("/publicadas")
     public ResponseEntity<?> getVagasPublicadas(){
@@ -26,49 +30,49 @@ public class VagaController {
         return ResponseEntity.ok(vagaService.listarVagasArquivadas());
     }
 
-    @GetMapping("/Rascunhos")
+    @GetMapping("/rascunhos")
     public ResponseEntity<?> getVagasRascunhos(){
         return ResponseEntity.ok(vagaService.listarVagasEmRascunho());
     }
 
-    @GetMapping("/Encerradas")
+    @GetMapping("/encerradas")
     public  ResponseEntity<?> getVagasEncerradas(){
         return ResponseEntity.ok(vagaService.listarVagasEncerrados());
     }
 
     @PostMapping
-    public ResponseEntity<?> cadastrarVaga(@RequestBody @Valid @NotBlank Vaga vaga){
-        vagaService.create(vaga);
+    public ResponseEntity<?> cadastrarVaga(@RequestBody @Valid CreateVagaDTO dto){
+        vagaService.create(dto);
         return ResponseEntity.ok("Vaga Cadastrada com sucesso!");
     }
 
     @PutMapping
-    public ResponseEntity<?> atualizarVaga(@RequestBody @Valid Vaga vaga){
+    public ResponseEntity<?> atualizarVaga(@RequestBody @Valid UpdateVagaDTO vaga){
         vagaService.update(vaga);
         return ResponseEntity.ok("Vaga Atualizada com sucesso!");
     }
 
-    @PutMapping("/publicar")
-    public ResponseEntity<?> publicarVaga(@RequestBody @Valid Vaga vaga){
-        vagaService.publicar(vaga);
+    @PutMapping("/{id}/publicar")
+    public ResponseEntity<?> publicarVaga(@PathVariable UUID id){
+        vagaService.publicar(id);
         return ResponseEntity.ok("Vaga Publicada com sucesso!");
     }
 
-    @PutMapping("/arquivar")
-    public ResponseEntity<?> arquivarVaga(@RequestBody @Valid Vaga vaga){
-        vagaService.arquivar(vaga);
+    @PutMapping("/{id}/arquivar")
+    public ResponseEntity<?> arquivarVaga(@PathVariable UUID id){
+        vagaService.arquivar(id);
         return ResponseEntity.ok("Vaga Arquiva com sucesso!");
     }
 
-    @PutMapping("/rascunho")
-    public ResponseEntity<?> salvarVaga(@RequestBody @Valid Vaga vaga){
-        vagaService.rascunho(vaga);
+    @PutMapping("/{id}/rascunho")
+    public ResponseEntity<?> salvarVaga(@PathVariable UUID id){
+        vagaService.rascunho(id);
         return ResponseEntity.ok("Vaga Salva em rascunho com sucesso!");
     }
 
-    @PutMapping("/encerrar")
-    public ResponseEntity<?> encerrarVaga(@RequestBody @Valid Vaga vaga){
-        vagaService.encerrar(vaga);
+    @PutMapping("/{id}/encerrar")
+    public ResponseEntity<?> encerrarVaga(@PathVariable UUID id){
+        vagaService.encerrar(id);
         return ResponseEntity.ok("Vaga Encerrada com sucesso!");
     }
 }
