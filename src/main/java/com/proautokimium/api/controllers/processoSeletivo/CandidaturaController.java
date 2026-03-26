3c459bf018d27bf499a8e4cacd109b1f00a6eebd
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-26 14:45:19 -0300

    feat: criado controllers para manipular candidatos e candidaturas

---
+++ src/main/java/com/proautokimium/api/controllers/processoSeletivo/CandidaturaController.java
@@ -0,0 +1,49 @@
+package com.proautokimium.api.controllers.processoSeletivo;
+
+import com.proautokimium.api.Application.DTOs.processoSeletivo.candidaturas.CreateCandidaturaDTO;
+import com.proautokimium.api.Infrastructure.services.processoSeletivo.CandidaturaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/candidatura")
+public class CandidaturaController {
+
+    private final CandidaturaService candidaturaService;
+
+    public CandidaturaController(CandidaturaService candidaturaService) {
+        this.candidaturaService = candidaturaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createCandidatura(@RequestBody CreateCandidaturaDTO dto){
+        candidaturaService.create(dto);
+        return ResponseEntity.ok("Candidatura realizada com sucesso");
+    }
+
+    @PutMapping("/{id}/avancar")
+    public ResponseEntity<?> avancarEtapa(@PathVariable UUID id){
+        candidaturaService.avancarEtapa(id);
+        return ResponseEntity.ok("Etapa avançada com sucesso");
+    }
+
+    @PutMapping("/{id}/aprovar")
+    public ResponseEntity<?> aprovarCandidato(@PathVariable UUID id){
+        candidaturaService.aprovarCandidatura(id);
+        return ResponseEntity.ok("Candidatura aprovada com sucesso");
+    }
+
+    @PutMapping("/{id}/reprovar")
+    public ResponseEntity<?> reprovarCandidato(@PathVariable UUID id){
+        candidaturaService.reprovarCandidatura(id);
+        return ResponseEntity.ok("Candidatura reprovada com sucesso");
+    }
+
+    @PutMapping("/{id}/encerrar")
+    public ResponseEntity<?> encerrarCandidato(@PathVariable UUID id){
+        candidaturaService.encerrarCandidatura(id);
+        return ResponseEntity.ok("Candidatura encerrada com sucesso");
+    }
+}
