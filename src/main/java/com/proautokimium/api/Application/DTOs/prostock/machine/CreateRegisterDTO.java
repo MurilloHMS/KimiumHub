908539dd307e4ac29cc74cf0b7db21f512c1575f
MurilloHMS <murillohenriquemelo@outlook.com>
2026-04-15 11:32:37 -0300

    feat: criado dto's

---
+++ src/main/java/com/proautokimium/api/Application/DTOs/prostock/machine/CreateRegisterDTO.java
@@ -0,0 +1,17 @@
+package com.proautokimium.api.Application.DTOs.prostock.machine;
+
+import com.proautokimium.api.domain.enums.MachineStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CreateRegisterDTO(
+        UUID machineId,
+        String nomeCliente,
+        short tag,
+        String solicitante,
+        MachineStatus status,
+        String Observacao,
+        LocalDateTime previsaoEntrega,
+        String tecnico
+) { }
