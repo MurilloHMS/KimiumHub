b3ef60bae7b52d7d839ca7d0731da0318ae7ba0c
MurilloHMS <murillohenriquemelo@outlook.com>
2026-04-15 12:11:59 -0300

    chore: removido validação do dto

--- src/main/java/com/proautokimium/api/Application/DTOs/prostock/machine/MachineDTO.java
+++ src/main/java/com/proautokimium/api/Application/DTOs/prostock/machine/MachineDTO.java
@@ -13,9 +13,7 @@ public record MachineDTO(
         String name,
         @NotBlank(message = "A marca é obrigatória")
         String brand,
-        @NotBlank(message = "O tipo de máquina é obrigatório")
         MachineType machineType,
-        @NotBlank(message = "O status da máquina é obrigatório")
         MachineStatus machineStatus,
         int minimum_stock,
         boolean active
