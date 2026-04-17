748d20fad5779a3af7712b88109dbf544c5a0dd8
MurilloHMS <murillohenriquemelo@outlook.com>
2026-04-17 16:26:41 -0300

    feat: finalizado configuração do scheduler

--- src/main/java/com/proautokimium/api/Infrastructure/services/email/smtp/SmtpService.java
+++ src/main/java/com/proautokimium/api/Infrastructure/services/email/smtp/SmtpService.java
@@ -1,19 +1,9 @@
 package com.proautokimium.api.Infrastructure.services.email.smtp;
 
 import com.proautokimium.api.domain.entities.email.EmailQueue;
-import jakarta.activation.DataHandler;
-import jakarta.activation.DataSource;
-import jakarta.activation.FileDataSource;
-import jakarta.mail.Message;
-import jakarta.mail.Multipart;
-import jakarta.mail.Transport;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeBodyPart;
-import jakarta.mail.internet.MimeMultipart;
 import jakarta.mail.util.ByteArrayDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -22,18 +12,18 @@ import org.springframework.web.multipart.MultipartFile;
 import com.proautokimium.api.Application.DTOs.smtp.SmtpMail;
 import jakarta.mail.internet.MimeMessage;
 
-import javax.xml.crypto.Data;
-import java.util.Objects;
-
 @Service
 public class SmtpService {
 
-	@Autowired
-	JavaMailSender mailSender;
+	private final JavaMailSender mailSender;
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(SmtpService.class);
 
-	public void sendEmail(SmtpMail request, MultipartFile[] attachments) {
+    public SmtpService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendEmail(SmtpMail request, MultipartFile[] attachments) {
 		try {
 
 			for (String recipient : request.recipients()) {
@@ -75,7 +65,7 @@ public class SmtpService {
 			MimeMessage message = mailSender.createMimeMessage();
 			MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-			helper.setFrom("seuemail@empresa.com", "Proauto Kimium");
+			helper.setFrom("noreply@envios.proautokimium.com.br", "Proauto Kimium");
 			helper.setTo(email.getToEmail());
 			helper.setSubject(email.getSubject());
 			helper.setText(email.getBody(), true);
@@ -87,7 +77,8 @@ public class SmtpService {
 			mailSender.send(message);
 
 		} catch (Exception e) {
-			throw new RuntimeException("Erro ao enviar email", e);
+			LOGGER.error("Erro ao enviar e-mail", e);
+			throw new RuntimeException(e);
 		}
 	}
 }
