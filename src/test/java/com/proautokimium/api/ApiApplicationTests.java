1d39812891e776d4ef9207831e930eead2561b3d
MurilloHMS <murillohenriquemelo@outlook.com>
2026-05-18 11:56:40 -0300

    chore(aplicationTest): corrigido teste e incluído test.properties

--- src/test/java/com/proautokimium/api/ApiApplicationTests.java
+++ src/test/java/com/proautokimium/api/ApiApplicationTests.java
@@ -1,10 +1,22 @@
 package com.proautokimium.api;
 
+import com.proautokimium.api.Infrastructure.services.email.smtp.SmtpService;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApiApplicationTests {
+
+	@MockitoBean
+	private SmtpService smtpService;
+
+	@MockitoBean
+	private JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
