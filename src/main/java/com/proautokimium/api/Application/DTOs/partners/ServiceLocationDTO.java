f6da906eab78ed8f709df54132f2725b688bc4b8
MurilloHMS <murillohenriquemelo@outlook.com>
2026-06-02 08:09:21 -0300

    chore: removido arquivos do controle dos veículos

--- src/main/java/com/proautokimium/api/Application/DTOs/partners/ServiceLocationDTO.java
+++
@@ -1,8 +0,0 @@
-package com.proautokimium.api.Application.DTOs.partners;
-
-public record ServiceLocationDTO(String codParceiro,
-                                 String documento,
-                                 String nome,
-                                 String email,
-                                 boolean ativo,
-                                 String address) {}
--- src/main/java/com/proautokimium/api/Infrastructure/repositories/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proautokimium.api.Infrastructure.repositories;
-
-import com.proautokimium.api.domain.entities.Vehicle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
-    Vehicle findVehicleByPlaca(String placa);
-}
