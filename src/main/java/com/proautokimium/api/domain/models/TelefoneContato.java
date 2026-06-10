af5b8a706a9dfbe8ca165a089fe3bda5c73e28d1
MurilloHMS <murillohenriquemelo@outlook.com>
2026-06-10 08:42:02 -0300

    chore: removido embedded

--- src/main/java/com/proautokimium/api/domain/models/TelefoneContato.java
+++ src/main/java/com/proautokimium/api/domain/models/TelefoneContato.java
@@ -1,12 +1,15 @@
 package com.proautokimium.api.domain.models;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Embeddable
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TelefoneContato {
     private String tipo;
     private String numero;
