c92a381c809584b9807d0f4fc771d70d8c0a6296
MurilloHMS <Murillohenriquemelo@outlook.com>
2026-07-06 09:30:15 -0300

    docs: incluído documentação

--- src/main/java/com/proautokimium/api/controllers/ContactController.java
+++ src/main/java/com/proautokimium/api/controllers/ContactController.java
@@ -2,6 +2,8 @@ package com.proautokimium.api.controllers;
 
 import com.proautokimium.api.Application.DTOs.contact.CreateContactDTO;
 import com.proautokimium.api.Infrastructure.services.ContactService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/contact")
+@Tag(name = "Contato empresa", description = "Cria e retorna os dados de contato da empresa")
 public class ContactController {
 
     @Autowired
@@ -26,7 +29,13 @@ public class ContactController {
         return ResponseEntity.ok(contacts);
     }
 
+    /**
+     * Cria um registro de contato.
+     * @param dto CreateContactDTO - Dto para criação de um novo registro de contato.
+     * @return HttpStatus Created (201)
+     */
     @PostMapping
+    @Operation(summary = "Registra um contato", description = "Recebe os dados de contato e registra")
     public ResponseEntity<Object> create(@RequestBody @NotNull @Valid CreateContactDTO dto){
         service.createContact(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
