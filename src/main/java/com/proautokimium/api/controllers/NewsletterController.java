a885a24389f1cf4093dbf6c68de38c1c63aeb2aa
MurilloHMS <Murillohenriquemelo@outlook.com>
2026-07-06 17:17:11 -0300

    docs: incluído documentação do swagger
    - Documentado controller
    - Documentado métodos do controller

--- src/main/java/com/proautokimium/api/controllers/NewsletterController.java
+++ src/main/java/com/proautokimium/api/controllers/NewsletterController.java
@@ -4,6 +4,8 @@ import com.proautokimium.api.Infrastructure.services.email.newsletter.Newsletter
 import com.proautokimium.api.Infrastructure.services.email.newsletter.NewsletterService;
 import com.proautokimium.api.domain.entities.Newsletter;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,12 +18,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/newsletter")
+@Tag(name = "Newsletter", description = "Controle das newsletters")
 public class NewsletterController {
     @Autowired
     private NewsletterService newsletterService;
@@ -30,6 +32,7 @@ public class NewsletterController {
     private NewsletterOrchestratorService newsletterOrchestratorService;
 
     @PostMapping("send")
+    @Operation(summary = "Envia Newsletter", description = "Envio Individual da Newsletter")
     public ResponseEntity<Object> sendNewsletter(@RequestBody Newsletter newsletter) throws MessagingException, UnsupportedEncodingException {
         newsletterService.sendMailWithInline(newsletter);
 
@@ -37,6 +40,7 @@ public class NewsletterController {
     }
     
     @PostMapping("upload")
+    @Operation(summary = "Cadastra Newsletters", description = "Recebe arquivos para montar newsletters")
     public ResponseEntity<Object> includeNewsletters(@RequestParam List<MultipartFile> files, @RequestParam(required = false) boolean isMatriz) throws Exception{
     	if(files.size() > 4) {
     		return ResponseEntity.badRequest().body("Máximo permitido 4 arquivos. Você enviou " + files.size());
@@ -47,12 +51,14 @@ public class NewsletterController {
     }
 
     @PostMapping("upload/one-file")
+    @Operation(summary = "Cadastra Newsletters", description = "Recebe arquivo único para montar newsletter")
     public ResponseEntity<Object> includeNewsletters(@RequestParam MultipartFile file) {
         newsletterOrchestratorService.includeMonthlyNewsletterByExcel(file);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
     
     @GetMapping("pending")
+    @Operation(summary = "Obtém Newsletters", description = "Retorna Newsletters pendentes de envio")
     public ResponseEntity<Object> getPendingEmails(){
     	var pendingEmails = newsletterService.getAllPendingEmails();
     	return pendingEmails != null ?
@@ -61,6 +67,7 @@ public class NewsletterController {
     }
     
     @PostMapping("pending/send")
+    @Operation(summary = "Envia Newsletters Pendentes", description = "Envio das Newsletters pendentes")
     public ResponseEntity<Object> sentPendingNewsletter(){
     	newsletterService.setReadyToSend();
     	return ResponseEntity.ok("Envio de newsletters iniciado.");
