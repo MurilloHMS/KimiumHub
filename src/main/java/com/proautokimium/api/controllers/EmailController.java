cb47cd9bc8caf86d0a77954d7d586831a1dbb311
MurilloHMS <Murillohenriquemelo@outlook.com>
2026-07-06 10:05:14 -0300

    docs(EmailController): documentado métodos do controller

--- src/main/java/com/proautokimium/api/controllers/EmailController.java
+++ src/main/java/com/proautokimium/api/controllers/EmailController.java
@@ -3,28 +3,46 @@ package com.proautokimium.api.controllers;
 import com.proautokimium.api.Application.DTOs.email.SmtpEmailRequestDTO;
 import com.proautokimium.api.Infrastructure.services.email.EmailService;
 import com.proautokimium.api.domain.entities.EmailEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
+/**
+ * Responsável pelo cadastro dos e-mails utilizados nos envios SMTP
+ */
 @RestController
 @RequestMapping("api/email")
+@Tag(name = "Cria emails SMTP", description = "Cria emails para utilizar no disparo smtp")
 public class EmailController {
 
     @Autowired
     private EmailService service;
 
+    /**
+     * Cria endereço de e-mail para envios SMTP
+     * @param dto Recebe nome do e-mail
+     * @return HttpStatus Created (201)
+     */
     @PostMapping
+    @Operation(summary = "Cria email", description = "Cria email para envio SMTP")
     public ResponseEntity<Object> createEmail(@RequestBody @NotNull @Valid SmtpEmailRequestDTO dto){
         service.saveEmail(dto);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /**
+     * Retorna lista de e-mails disponíveis
+     * @return Lista de Emails
+     */
     @GetMapping
+    @Operation(summary = "Obtém lista de Emails", description = "Retorna lista de e-mails disponíveis para envio SMTP")
     public ResponseEntity<Object> getAllEmails(){
         Set<EmailEntity> emails = service.getAll();
         return ResponseEntity.ok(emails);
