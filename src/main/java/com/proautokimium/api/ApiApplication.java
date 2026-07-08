647ba5f03ca8bbaacdac31d74b099a7ccc11c5bc
MurilloHMS <Murillohenriquemelo@outlook.com>
2026-07-08 12:42:14 -0300

    chore: incluído WebSecurity annotation

--- src/main/java/com/proautokimium/api/ApiApplication.java
+++ src/main/java/com/proautokimium/api/ApiApplication.java
@@ -3,9 +3,11 @@ package com.proautokimium.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableMethodSecurity
 public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
