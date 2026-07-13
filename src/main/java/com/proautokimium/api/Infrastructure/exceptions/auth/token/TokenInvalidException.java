126e63789a44aa9b7b3db27089962dfb4892c446
MurilloHMS <Murillohenriquemelo@outlook.com>
2026-07-13 10:22:59 -0300

    feat: criado novas exceções

---
+++ src/main/java/com/proautokimium/api/Infrastructure/exceptions/auth/token/TokenInvalidException.java
@@ -0,0 +1,10 @@
+package com.proautokimium.api.Infrastructure.exceptions.auth.token;
+
+import com.proautokimium.api.domain.exceptions.DomainException;
+import org.springframework.http.HttpStatus;
+
+public class TokenInvalidException extends DomainException {
+    public TokenInvalidException(String message) {
+        super(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
